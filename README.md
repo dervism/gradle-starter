6251f21258aa0d05fa6f7fd2390b71fd8e3a3ca9
dervism <dervis4@gmail.com>
2020-11-26 14:52:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Gradle Starter
+
+Sample Gradle starter project with Java & JUnit
+
+
+#### Build
+
+- gradle clean build
+
+#### Run the application
+
+- gradle run
---
+++ src/main/java/no/nav/bedpress/Kalkulator.java
@@ -0,0 +1,11 @@
+package no.nav.bedpress;
+
+public class Kalkulator {
+    public int pluss(int x, int y) {
+        return x + y;
+    }
+
+    public double dele(double x, double y) {
+        return x / y;
+    }
+}
